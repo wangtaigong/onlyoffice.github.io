c724c2263b37e78feade663c5cc27f464fb7cec9
wangtaigong <youaremynet@163.com>
2024-05-09 10:34:26 +0800

    test

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "https://wangtaigong.github.io/onlyoffice.github.io/sdkjs-plugins/content/html/";
+	var URL_TO_PLUGIN = "https://wangtaigong.github.io/onlyoffice.github.io/sdkjs-plugins/content/helloworld/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
